2adbfe1ea710726d9047e07100f3cf5b36736969
Nikolai Poriadin <np25071984@gmail.com>
2023-12-10 10:35:28 -0500

    Fix randomness

--- src/server/Randomizer.ts
+++ src/server/Randomizer.ts
@@ -57,13 +57,13 @@ class Randomizer
         for (const isHorizontal of orientations) {
             const maxCol = isHorizontal ? grid.cols - shipType.getSize() : grid.cols
             const maxRow = isHorizontal ? grid.rows : grid.rows - shipType.getSize()
-            const randomRowOffset = Math.floor(Math.random() * maxRow)
+            const randomRowOffset = Math.round(Math.random() * maxRow)
             for (var r = 0; r <= maxRow; r++) {
                 var rr = r + randomRowOffset
                 if (rr >= maxRow) {
                     rr -= maxRow
                 }
-                const randomColOffset = Math.floor(Math.random() * maxCol)
+                const randomColOffset = Math.round(Math.random() * maxCol)
                 for (var c = 0; c <= maxCol; c++) {
                     var cc = c + randomColOffset
                     if (cc >= maxCol) {
